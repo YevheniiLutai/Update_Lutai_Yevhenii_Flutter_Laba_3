219a4c8339202f14700302beba900a0c3af5d619
YevheniiLutai <53271797+YevheniiLutai@users.noreply.github.com>
2020-12-06 16:18:58 +0200

    'Finish'

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,7 @@ void main() => runApp(MyAdaptingApp());
 class MyAdaptingApp extends StatelessWidget {
   @override
   Widget build(context) {
-    // Either Material or Cupertino widgets work in either Material or Cupertino
-    // Apps.
+
     return MaterialApp(
       title: 'Adaptive Music App',
       theme: ThemeData(
@@ -23,9 +22,7 @@ class MyAdaptingApp extends StatelessWidget {
       darkTheme: ThemeData.dark(),
       builder: (context, child) {
         return CupertinoTheme(
-          // Instead of letting Cupertino widgets auto-adapt to the Material
-          // theme (which is green), this app will use a different theme
-          // for Cupertino (which is blue by default).
+
           data: CupertinoThemeData(),
           child: Material(child: child),
         );
